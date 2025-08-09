b857c65e7e1441cc0f2a5abfea96ddea917adea7
Wei-chih-han <weidan26504@gmail.com>
2025-08-09 13:43:49 +0800

    Revert "增加Swagger註解"
    This reverts commit 3d27fb7e90ee3feb7b5e8bbb1a03d8708f08f4f1.

--- src/main/java/com/gjun/controller/ProductController.java
+++ src/main/java/com/gjun/controller/ProductController.java
@@ -1,32 +1,23 @@
 package com.gjun.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.gjun.entity.Product;
 import com.gjun.service.ProductService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 @RestController
 @RequestMapping("api")
-@Tag(name = "會員操作", description = "提供會員新增、查詢等操作")
 public class ProductController {
 
-	@Autowired
-	private ProductService productService;
+  @Autowired
+  private ProductService productService;
 
-	@GetMapping("QueryProduct")
-	@ApiResponse(responseCode = "200", description = "查詢成功")
-	@Operation(summary = "取得所有會員清單")
-	public ResponseEntity<List<Product>> QueryProduct() {
-		return ResponseEntity.ok(productService.QueryProduct());
-	}
+  @GetMapping("QueryProduct")
+  public ResponseEntity<List<Product>> QueryProduct() {
+    return ResponseEntity.ok(productService.QueryProduct());
+  }
 }
