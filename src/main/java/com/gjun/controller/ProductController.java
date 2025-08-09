a1bb4a296fd195eea9c0faed774da7ea56ffa549
DESKTOP-P4UUKAO\student <bert0210au@gmail.com>
2025-08-09 11:32:07 +0800

    增加Swagger註解

--- src/main/java/com/gjun/controller/ProductController.java
+++ src/main/java/com/gjun/controller/ProductController.java
@@ -1,23 +1,32 @@
 package com.gjun.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.gjun.entity.Product;
 import com.gjun.service.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "介面操作", description = "提供產品查詢等操作")
 @RestController
 @RequestMapping("api")
 public class ProductController {
 
-  @Autowired
-  private ProductService productService;
+	@Autowired
+	private ProductService productService;
 
-  @GetMapping("QueryProduct")
-  public ResponseEntity<List<Product>> QueryProduct() {
-    return ResponseEntity.ok(productService.QueryProduct());
-  }
+	@Operation(summary = "取得所有清單")
+	@ApiResponse(responseCode = "200", description = "查詢成功")
+	@GetMapping("QueryProduct")
+	public ResponseEntity<List<Product>> QueryProduct() {
+		return ResponseEntity.ok(productService.QueryProduct());
+	}
 }
