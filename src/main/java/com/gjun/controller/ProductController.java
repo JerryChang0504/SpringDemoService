1e9a4c5098f7e557822762407feaf0b302cd1b47
JerryChang0504 <fx2ptt@gmail.com>
2025-08-09 14:24:16 +0800

    Revert "Merge remote-tracking branch 'origin/dev_weft71500' into dev_Jerry"
    This reverts commit 12d3936a2ed616679f014f1a103016b301d9d094, reversing
    changes made to fa3bfc16a036b3586e28f8e392c2b2826a35dd8c.

--- src/main/java/com/gjun/controller/ProductController.java
+++ src/main/java/com/gjun/controller/ProductController.java
@@ -3,17 +3,16 @@ package com.gjun.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gjun.entity.Product;
 import com.gjun.service.ProductService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -25,12 +24,9 @@ public class ProductController {
 	private ProductService productService;
 
 	@GetMapping("QueryProduct")
+	@ApiResponse(responseCode = "200", description = "查詢成功")
+	@Operation(summary = "取得所有會員清單")
 	public ResponseEntity<List<Product>> QueryProduct() {
 		return ResponseEntity.ok(productService.QueryProduct());
-	}
-
-	@PostMapping("/student")
-	public ResponseEntity<String> responseEntity(@RequestBody Student student) {
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body("Hello");
 	}
 }
