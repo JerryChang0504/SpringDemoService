5c7a79cc156ec65ef003ee295defdce73974b58a
Wei-chih-han <weidan26504@gmail.com>
2025-08-09 13:44:16 +0800

    Revert "@Schema(description = "產品名稱", example = "電腦")"
    This reverts commit 3cdc1846312d492d46665c2984363377a3632fef.

--- src/main/java/com/gjun/entity/Product.java
+++ src/main/java/com/gjun/entity/Product.java
@@ -1,6 +1,5 @@
 package com.gjun.entity;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,22 +17,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Table(name = "product")
-@Schema(description = "會員資料傳入值")
 public class Product extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "product_id")
-	private Integer id;
-
-	@Schema(description = "產品名稱", example = "電腦")
-	@Column(name = "product_name")
-	private String name;
-	@Column(name = "product_description")
-	private String description;
-	@Column(name = "product_price")
-	private Integer price;
-	@Column(name = "product_image")
-	private String image;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "product_id")
+  private Integer id;
+  @Column(name = "product_name")
+  private String name;
+  @Column(name = "product_description")
+  private String description;
+  @Column(name = "product_price")
+  private Integer price;
+  @Column(name = "product_image")
+  private String image;
 
 }
