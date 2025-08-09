903b8e96a8781c9b0e567a8f80fdb35d94a6cc63
DESKTOP-P4UUKAO\student <52mattin199809214@gmail.com>
2025-08-09 12:12:22 +0800

    @RequestMapping("apiii")

--- src/main/java/com/gjun/controller/ProductController.java
+++ src/main/java/com/gjun/controller/ProductController.java
@@ -1,23 +1,25 @@
 package com.gjun.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.gjun.entity.Product;
 import com.gjun.service.ProductService;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("apiii")
 public class ProductController {
 
-  @Autowired
-  private ProductService productService;
+	@Autowired
+	private ProductService productService;
 
-  @GetMapping("QueryProduct")
-  public ResponseEntity<List<Product>> QueryProduct() {
-    return ResponseEntity.ok(productService.QueryProduct());
-  }
+	@GetMapping("QueryProduct")
+	public ResponseEntity<List<Product>> QueryProduct() {
+		return ResponseEntity.ok(productService.QueryProduct());
+	}
 }
