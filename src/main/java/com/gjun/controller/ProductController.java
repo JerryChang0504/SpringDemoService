8ca643c7e116fb5bae749c74a24448cd25012aff
DESKTOP-P4UUKAO\student <weft71500@gmail.com>
2025-08-09 12:02:54 +0800

    新增PostMapping

--- src/main/java/com/gjun/controller/ProductController.java
+++ src/main/java/com/gjun/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.gjun.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,10 @@ public class ProductController {
   @GetMapping("QueryProduct")
   public ResponseEntity<List<Product>> QueryProduct() {
     return ResponseEntity.ok(productService.QueryProduct());
+  }
+  
+  @PostMapping("/student")
+  public ResponseEntity<String> responseEntity(@RequestBody Student student){
+	  return ResponseEntity.status(HttpStatus.ACCEPTED).body("Hello");
   }
 }
